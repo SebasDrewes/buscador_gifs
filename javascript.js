cb860d7a4dea993f6c32ab5e2289aa607103ef85
Sebastian Drewes <sebas.drewes@gmail.com>
2021-06-22 18:01:57 -0300

    refactored to async await to practice

--- javascript.js
+++ javascript.js
@@ -1,17 +1,16 @@
 const img = document.querySelector('img');
-const loadImage = (value) => {
-  if (value) {
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=R7F7GZ7VzLKhFYXgldz1WhPMJCVLS7H5&s=${value}`, { mode: 'cors' })
-      .then((response) => response.json())
-      .then((response) => {
-        img.src = response.data.images.original.url;
-      })
-      .catch(() => {
-        img.src = 'img/notfound.png';
-      });
+const loadImage = async (value) => {
+  try {
+    if (value) {
+      const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=R7F7GZ7VzLKhFYXgldz1WhPMJCVLS7H5&s=${value}`, { mode: 'cors' });
+      const gifData = await response.json();
+      img.src = gifData.data.images.original.url;
     // en caso de que el buscador este vacio, busca labradores
-  } else {
-    loadImage('labrador');
+    } else {
+      loadImage('labrador');
+    }
+  } catch {
+    img.src = 'img/notfound.png';
   }
 };
 const buscar = document.querySelector('#buscar');
